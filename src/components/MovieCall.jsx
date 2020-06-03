6bff2fa7e5e2b2b8c1c574d615d5f3d55c9d7310
Rand al'Thor <alexanderhogle@gmail.com>
2020-06-03 11:30:43 -0500

    changes moviecall form text

--- src/components/MovieCall.jsx
+++ src/components/MovieCall.jsx
@@ -5,31 +5,39 @@ function MovieCall() {
   return (
     <div className="container login">
       <form className="form-signin">
-        <i class="fas fa-ghost fa-5x"></i>
+        <h5>Find A Movie</h5>
         <Input
-          name={"Email address"}
-          for={"inputEmail"}
-          type={"email"}
-          id={"inputEmail"}
-          placeholder={"Email address"}
+          name={"Title"}
+          for={"inputTitle"}
+          type={"text"}
+          id={"inputTitle"}
+          placeholder={"Title"}
           required={""}
           autoComplete={"off"}
         />
         <Input
-          name={"Password"}
-          for={"inputPassword"}
-          type={"password"}
+          name={"Director"}
+          for={"inputDirector"}
+          type={"text"}
           id={"inputPassword"}
-          placeholder={"Password"}
+          placeholder={"Director"}
           required={""}
           autoComplete={"off"}
         />
-
+        <Input
+          name={"Year"}
+          for={"inputYear"}
+          type={"text"}
+          id={"inputYear"}
+          placeholder={"Year"}
+          required={""}
+          autoComplete={"off"}
+        />
         <button
           className="btn btn-lg btn-primary btn-block custom-btn"
           type="submit"
         >
-          Sign in
+          Search
         </button>
       </form>
     </div>
